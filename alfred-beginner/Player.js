4719366de083d2cb18af18038e995788be0fe49e
AlfredJKwack <thomas+github@badsoda.com>
2018-06-05 23:09:59 +0200

    Level 4 completed
    Success! You have found the stairs.
    Warrior Score: 55
    Time Bonus: 10
    Clear Bonus: 13
    Total Score: 97 + 78 = 175

--- alfred-beginner/Player.js
+++ alfred-beginner/Player.js
@@ -2,19 +2,28 @@ class Player {
   playTurn(warrior) {
 
   	if(!this._init){
-  		warrior.think('is tying his shoelaces.');
+  		warrior.think('tying his shoelaces is necessary.');
   		this._prevHealth = warrior.health();
   		this._maxHealth  = warrior.health();
   		this._init       = true;
   	}
-  	if(warrior.health() < this._maxHealth && warrior.feel().isEmpty()){
-  		warrior.think('he should take a knee.');
+
+  	if(warrior.health() < this._prevHealth && warrior.feel().isEmpty()){
+  		warrior.think('he cannot rest here and should press on.');
+  		warrior.walk();
+  	}
+
+  	else if(warrior.health() < this._maxHealth && warrior.feel().isEmpty()){
+  		warrior.think('it is safe to take a knee.');
   		warrior.rest();
   	}
+
     else if(warrior.feel().isEmpty()){
     	warrior.think('the road ahead is clear.');
     	warrior.walk();
-    } else {
+    } 
+
+    else {
     	warrior.think('his steel is sharp.');
     	warrior.attack();
     }
